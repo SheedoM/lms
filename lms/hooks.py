45d9e579485802b3b1d3439f684e80e3902eaffb
Shady Faragallah <111459312+SheedoM@users.noreply.github.com>
2026-08-01 07:59:41 +0300

    route public subscription requests

--- lms/hooks.py
+++ lms/hooks.py
@@ -183,6 +183,7 @@ website_route_rules = [
 	{"from_route": f"/{get_lms_path()}/<path:app_path>", "to_route": "_lms"},
 	{"from_route": f"/{get_lms_path()}", "to_route": "_lms"},
 	{"from_route": "/courses/<slug>", "to_route": "course"},
+	{"from_route": "/subscribe/<slug>", "to_route": "subscribe"},
 	{
 		"from_route": "/courses/<course_name>/<certificate_id>",
 		"to_route": "certificate",
